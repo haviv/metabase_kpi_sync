1160dbc32adbba8f9af77662eb9fc5cb8482e1ff
Haviv Rosh <haviv.rosh@pathlock.com>
2025-12-20 17:50:06 +0200

    new snapshots

--- export_ado.py
+++ export_ado.py
@@ -458,7 +458,9 @@ class DatabaseConnection:
             ('open_bugs_QA_p1_p2', "SELECT COUNT(*) FROM bugs WHERE state IN ('Ready for QA', 'In QA') AND severity IN ('1','2')"),
             ('total_customers', " SELECT COUNT(DISTINCT i.customer_name) AS total_customers FROM issues i"),
             ('redline_bugs', "SELECT  COUNT(*) * 1.0 / (SELECT COUNT(DISTINCT customer_name) FROM bugs) AS bugs_with_parent_per_customer FROM bugs WHERE parent_issue IS NOT NULL AND DATE_TRUNC('month', created_date) = DATE_TRUNC('month', CURRENT_DATE)"),
-            ('open_p1_bugs_with_customer_issues', "SELECT COUNT(*) FROM bugs WHERE state NOT IN ('Done', 'Not reproduced', 'QA Completed', 'Removed') AND severity = '1' AND parent_issue IS NOT NULL")
+            ('open_p1_bugs_with_customer_issues', "SELECT COUNT(*) FROM bugs WHERE state NOT IN ('Done', 'Not reproduced', 'QA Completed', 'Removed') AND severity = '1' AND parent_issue IS NOT NULL"),
+            ('open_p1_bugs_with_customer_issues_cn', "SELECT COUNT(*) FROM bugs WHERE state NOT IN ('Done', 'Not reproduced', 'QA Completed', 'Removed') AND severity = '1' AND customer_name IS NOT NULL AND created_date >= '2025-12-01'"),
+            ('p1_p2_bugs_with_customer_issues_cn', "SELECT COUNT(*) FROM bugs WHERE state NOT IN ('Done', 'Not reproduced', 'QA Completed', 'Removed') AND severity IN ('1','2') AND customer_name IS NOT NULL AND created_date >= '2025-12-01'")
         ]
 
         snapshot_date = datetime.now().date()
